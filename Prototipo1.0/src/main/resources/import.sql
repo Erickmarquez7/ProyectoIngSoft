08d83a649ef6fbfb362d7311c5f5f9385a395f35
Erick B <erick07@ciencias.unam.mx>
2022-11-14 13:02:20 -0600

    ejemplo de rentar

--- Prototipo1.0/src/main/resources/import.sql
+++ Prototipo1.0/src/main/resources/import.sql
@@ -1,3 +1,5 @@
 INSERT INTO productos(idproducto, categoria, nombre, descripcion, cantidad, precio, dias) VALUES ('12345','Disco', 'CD José Madero', 'Disco psalmos el mejor perro disco', 100, 250.00, 5 )
 
 INSERT INTO usuarios(nocuenta, contrasena, nombre, paterno, materno, carrera, celular, correo, rol, activo) VALUES ('123456789', 'contrasena', 'nombre', 'paterno', 'materno', 'compu', 5555, 'correo', 1, true)
+
+INSERT INTO rentar(fechainicio, fechafin, nocuenta, idproducto) VALUES('2020-11-12','2021-12-07','123456789','12345')
