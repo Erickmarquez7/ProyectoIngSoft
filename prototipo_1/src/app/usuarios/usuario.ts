b1c08b7efcaa27f4f6dba9a10fb8d3bcfc39aa6c
Erick B <erick07@ciencias.unam.mx>
2022-11-15 14:19:32 -0600

    front usuarios

---
+++ prototipo_1/src/app/usuarios/usuario.ts
@@ -0,0 +1,15 @@
+export class Usuario {
+
+    id:number;
+    username:string;
+    password:string;
+
+    nombre:string;
+    paterno:string;
+    materno:string;
+    carrera:string;
+    celular: number;
+    email:string;
+    roles:string[]=[]
+
+}
