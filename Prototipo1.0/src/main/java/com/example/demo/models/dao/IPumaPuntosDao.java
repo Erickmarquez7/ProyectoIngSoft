e127efff75f0cffbd30a621daaeedcdb5b7c613a
dylan-radilla <41594377+dylan-radilla@users.noreply.github.com>
2022-11-17 13:07:14 -0600

    Delete Prototipo1.0/src directory

--- Prototipo1.0/src/main/java/com/example/demo/models/dao/IPumaPuntosDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.models.entity.PumaPuntos;
-
-public interface IPumaPuntosDao extends CrudRepository<PumaPuntos, Long> {
-
-}
